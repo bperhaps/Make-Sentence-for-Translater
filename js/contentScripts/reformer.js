9cf66c935b3f74571b0683264246da52c9f0f426
Minsung Son <sns2831@gmail.com>
2020-07-20 19:44:53 +0900

    reformer.js: modify Reduce the source.

--- js/contentScripts/reformer.js
+++ js/contentScripts/reformer.js
@@ -5,17 +5,6 @@ support = {
 	"papago" : "txtSource"
 };
 
-let eventHandler = null;
-let getEventHandler = (dom) => {
-    if(eventHandler == null) {
-        eventHandler = () => {
-            func(dom);
-        }
-        return eventHandler;
-    }
-    return eventHandler;
-}
-
 let getDomId = () => {
     let result = null;
     Object.keys(support).forEach((s) => {
@@ -27,6 +16,10 @@ let getDomId = () => {
     return result;
 }
 
+let reform = () => {
+    let dom = document.getElementById(getDomId());
+    dom.value = dom.value.replace(/[\r\n]/g, " ");
+}
 
 let listnerCtrl = (action) => {
     let dom = document.getElementById(getDomId()); 
@@ -34,19 +27,10 @@ let listnerCtrl = (action) => {
 
     switch(action) {
         case "set":
-            dom.addEventListener("input", getEventHandler(dom));
+            dom.addEventListener("input", reform);
             break;
         case "delete":
-            dom.removeEventListener("input", getEventHandler(dom));
+            dom.removeEventListener("input", reform);
             break;
     }
 };
-
-let reform = () => {
-    let dom = document.getElementById(getDomId());
-    func(dom);
-}
-
-let func = (dom) => {
-    dom.value = dom.value.replace(/[\r\n]/g, " ");
-}
