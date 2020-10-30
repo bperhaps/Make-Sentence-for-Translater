872a8695bed164ab0e026d9ba8e8e2c20f15cb16
Minsung Son <sns2831@gmail.com>
2020-10-30 09:18:50 +0900

    Update get textarea dom

--- js/contentScripts/reformer.js
+++ js/contentScripts/reformer.js
@@ -1,28 +1,12 @@
 url = window.location.hostname;
-	
-support = {
-	"google" : "source", 
-	"papago" : "txtSource"
-};
-
-let getDomId = () => {
-    let result = null;
-    Object.keys(support).forEach((s) => {
-        if(url.indexOf(s) != -1) {
-            result = support[s];
-            return;
-        };
-    })
-    return result;
-}
 
 let reform = () => {
-    let dom = document.getElementById(getDomId());
+    let dom = document.getElementsByTagName("textarea")[0];
     dom.value = dom.value.replace(/[\r\n]/g, " ");
 }
 
 let listnerCtrl = (action) => {
-    let dom = document.getElementById(getDomId()); 
+    let dom = document.getElementById(getDomId());
     if(dom == null) return;
 
     switch(action) {
